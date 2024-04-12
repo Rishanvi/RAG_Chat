9b03b0ce2a15077bd75febc3f7f4f5e37f821caa
Rishanvi <harishshanvi@gmail.com>
2024-04-12 17:05:31 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from indexing import query_translation
+from indexing.query_translation.decomposition import d_display
+from langsmith import Client
+import os
+os.environ["LANGCHAIN_API_KEY"] = "ls__08d1b346dd804c139717ead521d66221"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = "rag_chat model"
 
-
-
-
+Client()
 
 
 
 
 
 if(__name__=="__main__"):
-    print(query_translation.__doc__)
+   
     query=input("say something......")
-    result=query_translation.m_display(query)
+    result= d_display(query)
     print(result)
 
 
